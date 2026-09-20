9b47fc03cfc7ad1386106fc15d1ec1b33dbdf71d
mrwhite879 <229014913+mrwhite879@users.noreply.github.com>
2026-09-20 14:31:58 -0300

    Dia 7: Uso del break para terminar el bucle, y validacion

--- src/Main.java
+++ src/Main.java
@@ -21,43 +21,52 @@ public class Main {
         while (true) {
             System.out.println("Elija una opcion");
             System.out.println("1-Buscar libro");
-
+            System.out.println("2-Salir");
             if (teclado.hasNextInt()) {
                 opcion = teclado.nextInt();
+
                 if (opcion >= 1 && opcion <=5){
-                    break;
+                    System.out.println("Numero valido");
+                } else {
+                    System.out.println("Solo puede ingresar números del 1 al 5.");
                 }
 
             } else {
-                teclado.next();
+
+                System.out.println("Tipo de dato invalido.");
+
             }
 
+            teclado.nextLine();
             switch (opcion) {
 
                 case 1 -> {
                     System.out.println("Ingrese el nombre del libro");
                     String busqueda = teclado.nextLine();
-
+                    System.out.println(busqueda);
                     for (Book book : Libreria) {
                         String libroActual = book.getNombre();
                         String textoBusqueda = busqueda;
                         boolean encontrado = libroActual.toLowerCase().contains(textoBusqueda.toLowerCase());
                         if (encontrado) {
                             System.out.println("Libro encontrado "+ book.getNombre() + " de " + book.getAutor());
+
                         }
                     }
 
-
-
                 }
 
+                case 2 -> {
+                    System.out.println("Saliendo del programa...");
+                }
             }
+            break;
 
         }
 
 
 
-        teclado.close();
+
         }
 
     }
